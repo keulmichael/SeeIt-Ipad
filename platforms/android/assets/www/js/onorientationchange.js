1292b366bea9e21c43091b350ac7b4812920ad00
Host <host.michael@gmail.com>
2015-08-13 22:07:47 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -1,9 +1,28 @@
-WINDOW.onorientationchange = function() {
-   var portraitOrientation = window.matchMedia("(orientation:portrait)");
-   if ( portraitOrientation.matches ) {alert("(orientation:portrait)");
-document.getElementById('photoaccueil_1').offsetWidth = 45%;
-   } else {alert("(orientation:paysage)");
-document.getElementById('photoaccueil_1').offsetWidth = 65 %;	
-   }
- }
- 
+
+ // Find matches
+var mql = window.matchMedia("(orientation: portrait)");
+
+// If there are matches, we're in portrait
+if(mql.matches) {  
+	// Portrait orientation
+	alert("(orientation:portrait)");
+	document.getElementById('photoaccueil_1').offsetWidth = 45%;
+} else {  
+	// Landscape orientation
+	alert("(orientation:paysage)");
+	document.getElementById('photoaccueil_1').offsetWidth = 65 %;
+}
+
+// Add a media query change listener
+mql.addListener(function(m) {
+	if(m.matches) {
+		// Changed to portrait
+	alert("(orientation:portrait)");
+	document.getElementById('photoaccueil_1').offsetWidth = 45%;
+	}
+	else {
+		// Changed to landscape
+	alert("(orientation:paysage)");
+	document.getElementById('photoaccueil_1').offsetWidth = 65 %;
+	}
+});
