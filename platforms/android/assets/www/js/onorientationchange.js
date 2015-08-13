035f6bdc714eb3d02c8fc8431d47e28888cef303
Host <host.michael@gmail.com>
2015-08-13 22:00:30 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -1,8 +1,9 @@
 WINDOW.onorientationchange = function() {
    var portraitOrientation = window.matchMedia("(orientation:portrait)");
-   if ( portraitOrientation.matches ) {
+   if ( portraitOrientation.matches ) {alert("(orientation:portrait)");
 document.getElementById('photoaccueil_1').offsetWidth = 45%;
-   } else {
+   } else {alert("(orientation:paysage)");
 document.getElementById('photoaccueil_1').offsetWidth = 65 %;	
    }
  }
+ 
