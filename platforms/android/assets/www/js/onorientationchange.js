fa887729f985096c11898994a21ed3cfbfaba5ff
Host <host.michael@gmail.com>
2015-08-13 22:17:21 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -1,28 +1,21 @@
-
- // Find matches
-var mql = window.matchMedia("(orientation: portrait)");
-
-// If there are matches, we're in portrait
-if(mql.matches) {  
-	// Portrait orientation
-	alert("(orientation:portrait)");
-	document.getElementById('photoaccueil_1').offsetWidth = 45%;
-} else {  
-	// Landscape orientation
-	alert("(orientation:paysage)");
+           
+  function doOnOrientationChange()
+  {
+    switch( WINDOW.orientation) 
+    {  
+      case -90:
+      case 90:
+        alert('landscape');
 	document.getElementById('photoaccueil_1').offsetWidth = 65 %;
-}
+        break; 
+      default:
+        alert('portrait');
+      	document.getElementById('photoaccueil_1').offsetWidth = 45%;
+        break; 
+    }
+  }
 
-// Add a media query change listener
-mql.addListener(function(m) {
-	if(m.matches) {
-		// Changed to portrait
-	alert("(orientation:portrait)");
-	document.getElementById('photoaccueil_1').offsetWidth = 45%;
-	}
-	else {
-		// Changed to landscape
-	alert("(orientation:paysage)");
-	document.getElementById('photoaccueil_1').offsetWidth = 65 %;
-	}
-});
+ WINDOW.addEventListener('orientationchange', doOnOrientationChange);
+
+  // Initial execution if needed
+  //doOnOrientationChange();
