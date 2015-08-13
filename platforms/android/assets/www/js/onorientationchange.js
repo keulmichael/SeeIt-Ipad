2145dddffd844a5c8f6935a32cbc0152c16a0bf7
Host <host.michael@gmail.com>
2015-08-13 23:04:23 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -1,25 +1,8 @@
            
   function doOnOrientationChange()
   {
-    switch( WINDOW.orientation) 
-    {  
-      case -90:
-	              alert('landscape');
+    alert( WINDOW.orientation) ;
 
-        break; 
-      case 90:
-        alert('landscape');
-
-        break; 
-            case 0:
-        alert('portrait');
-
-        break; 
-      default:
-        alert('portrait');
-
-        break; 
-    }
   }
 
  WINDOW.addEventListener('orientationchange', doOnOrientationChange);
