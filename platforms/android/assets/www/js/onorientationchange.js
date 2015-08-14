8f1c587a36cb100cf04dbb5481773e55f0648e3f
Host <host.michael@gmail.com>
2015-08-14 11:14:50 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -1,14 +1,14 @@
   function listenerTailleEcran(requeteMedia) {
 	var orientationPortrait = document.getElementById('orientationPortrait');
 	if (requeteMedia.matches) {  
-	document.getElementById('containerAccueil').style.width = '500px';
+	document.getElementById('containerAccueil').style.height = '500px';
       	document.getElementById('photoaccueil_1').style.width = '100%';
       	document.getElementById('photoaccueil_2').style.width = '100%';
       	document.getElementById('photoaccueil_3').style.width = '100%';
       	document.getElementById('pubaccueil_1').style.width = '100%';
       	document.getElementById('pubaccueil_2').style.width = '100%';
 	} else {  
-	document.getElementById('containerAccueil').style.width = '300px';
+	document.getElementById('containerAccueil').style.height = '300px';
 	document.getElementById('photoaccueil_1').style.width = '45%';
 	document.getElementById('photoaccueil_2').style.width = '45%';
 	document.getElementById('photoaccueil_3').style.width = '45%';
