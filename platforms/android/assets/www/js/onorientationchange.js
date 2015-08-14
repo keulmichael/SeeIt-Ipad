800743f88c659414975ad7e1f340a8332f37b2d9
Host <host.michael@gmail.com>
2015-08-14 10:35:28 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -1,11 +1,17 @@
-           
-  function doOnOrientationChange()
-  {
-    alert( WINDOW.orientation) ;
-
-  }
-
- WINDOW.addEventListener('orientationchange', doOnOrientationChange);
-
-  // Initial execution if needed
-  doOnOrientationChange();
+  function listenerTailleEcran(requeteMedia) {
+	var orientationPortrait = document.getElementById('orientationPortrait');
+	if (requeteMedia.matches) {  
+        alert('portrait');
+      	document.getElementById('photoaccueil_1').offsetWidth = 45%;
+	} else {  
+        alert('landscape');
+	document.getElementById('photoaccueil_1').offsetWidth = 65 %;  
+	}
+}  
+	
+document.addEventListener('DOMContentLoaded', function(){
+	var requeteMedia = window.matchMedia("(orientation : portrait)");
+	requeteMedia.addListener(listenerTailleEcran);
+	
+	listenerTailleEcran(requeteMedia);
+}, false);
