402904debfab15a9e1ce738106f1843b67764981
Host <host.michael@gmail.com>
2015-08-19 21:57:08 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -1,16 +1,16 @@
   function listenerTailleEcran(requeteMedia) {
 	var orientationPortrait = document.getElementById('orientationPortrait');
 	if (requeteMedia.matches) {  
-	document.getElementById('containerAccueil').style.height = '300px';
-	document.getElementById('icone-seeit.png').style.width = '250px';	
+	document.getElementById('containerAccueil').style.height = '400px';
+	document.getElementById('icone-seeit.png').style.width = '350px';	
       	document.getElementById('photoaccueil_1').style.width = '70%';
       	document.getElementById('photoaccueil_2').style.width = '70%';
       	document.getElementById('photoaccueil_3').style.width = '70%';
       	document.getElementById('pubaccueil_1').style.width = '70%';
       	document.getElementById('pubaccueil_2').style.width = '70%';
 	} else {  
-	document.getElementById('containerAccueil').style.height = '300px';
-	document.getElementById('icone-seeit.png').style.width = '250px';
+	document.getElementById('containerAccueil').style.height = '400px';
+	document.getElementById('icone-seeit.png').style.width = '350px';
 	document.getElementById('photoaccueil_1').style.width = '45%';
 	document.getElementById('photoaccueil_2').style.width = '45%';
 	document.getElementById('photoaccueil_3').style.width = '45%';
