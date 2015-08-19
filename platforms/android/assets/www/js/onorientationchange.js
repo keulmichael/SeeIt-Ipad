e6b2fde61996a28741a8e8b02a7e732812ee83c4
Host <host.michael@gmail.com>
2015-08-19 18:53:15 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -1,13 +1,13 @@
   function listenerTailleEcran(requeteMedia) {
 	var orientationPortrait = document.getElementById('orientationPortrait');
 	if (requeteMedia.matches) {  
-	document.getElementById('containerAccueil').style.height = '450px';
-	document.getElementById('icone-seeit.png').style.width = '420px';	
-      	document.getElementById('photoaccueil_1').style.width = '93%';
-      	document.getElementById('photoaccueil_2').style.width = '93%';
-      	document.getElementById('photoaccueil_3').style.width = '93%';
-      	document.getElementById('pubaccueil_1').style.width = '93%';
-      	document.getElementById('pubaccueil_2').style.width = '93%';
+	document.getElementById('containerAccueil').style.height = '300px';
+	document.getElementById('icone-seeit.png').style.width = '250px';	
+      	document.getElementById('photoaccueil_1').style.width = '70%';
+      	document.getElementById('photoaccueil_2').style.width = '70%';
+      	document.getElementById('photoaccueil_3').style.width = '70%';
+      	document.getElementById('pubaccueil_1').style.width = '70%';
+      	document.getElementById('pubaccueil_2').style.width = '70%';
 	} else {  
 	document.getElementById('containerAccueil').style.height = '300px';
 	document.getElementById('icone-seeit.png').style.width = '250px';
