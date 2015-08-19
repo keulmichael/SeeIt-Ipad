fb63b7808509e1b0b60848508f99095f5e68a74f
Host <host.michael@gmail.com>
2015-08-19 22:21:26 +0200

    Update onorientationchange.js

--- platforms/android/assets/www/js/onorientationchange.js
+++ platforms/android/assets/www/js/onorientationchange.js
@@ -3,11 +3,11 @@
 	if (requeteMedia.matches) {  
 	document.getElementById('containerAccueil').style.height = '500px';
 	document.getElementById('icone-seeit.png').style.width = '450px';	
-      	document.getElementById('photoaccueil_1').style.width = '70%';
-      	document.getElementById('photoaccueil_2').style.width = '70%';
-      	document.getElementById('photoaccueil_3').style.width = '70%';
-      	document.getElementById('pubaccueil_1').style.width = '70%';
-      	document.getElementById('pubaccueil_2').style.width = '70%';
+      	document.getElementById('photoaccueil_1').style.width = '80%';
+      	document.getElementById('photoaccueil_2').style.width = '80%';
+      	document.getElementById('photoaccueil_3').style.width = '80%';
+      	document.getElementById('pubaccueil_1').style.width = '80%';
+      	document.getElementById('pubaccueil_2').style.width = '80%';
 	} else {  
 	document.getElementById('containerAccueil').style.height = '400px';
 	document.getElementById('icone-seeit.png').style.width = '350px';
