76faeddce000f1daa3962580da40c62c56df4691
Host <host.michael@gmail.com>
2015-08-25 23:51:44 +0200

    Create websocket.js

---
+++ platforms/android/assets/www/js/websocket.js
@@ -0,0 +1,33 @@
+var ws = new WebSocket('ws://websocket-seeit.rhcloud.com:8000');
+	    
+ws.onmessage = function (event) { 
+ var data = JSON.parse(event.data);
+    switch(data.type)
+    {
+       case "nouvelle connexion":
+       var id = Math.floor(Math.random() * 100000000000000) ;
+       sessionStorage.id = id;
+        alert("Bienvenue au nouvel utilisateur !");
+       
+       break;
+       case "confirmation photo":
+       	if (data.id != sessionStorage.id){
+	
+       	    	navigator.vibrate([100, 100, 100, 100, 100]);navigator.notification.beep(1);
+       	       alert("Nouvelle photo prise " + data.lieu);
+       	    
+       	}
+           break;
+       case "confirmation message":
+           alert(data.message);
+           break
+       case "confirmation ouvrir appareil photo":
+          onCapture();
+           break;
+    }
+};
+
+function Envoi_photo(){
+var data = { "type" : "envoi photo", "message" : "" };
+var message = JSON.stringify(data);
+ws.send(message);}
